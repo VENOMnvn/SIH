e781964dc214f924a0b1c7bd8703bf76d8f9a3fa
lalit6393 <amanengineer132@gmail.com>
2023-09-15 10:46:45 +0530

    f

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Notifications from "./components/Notifications";
+import { UserAuthProvider } from "./context/userContext";
+
+const LazySignup = lazy(() => import("./components/signup/Signup"));
+const LazyLogin = lazy(() => import("./components/login/Login"));
 
 function App() {
   return (
     <div className="App">
-        
+      <UserAuthProvider>
+        <Notifications />
+        <Routes>
+          <Route
+            path="login"
+            element={
+              <Suspense fallback="Loading...">
+                <LazyLogin />
+              </Suspense>
+            }
+          />
+          <Route
+            path="signup"
+            element={
+              <Suspense fallback="Loading...">
+                <LazySignup />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </UserAuthProvider>
     </div>
   );
 }
